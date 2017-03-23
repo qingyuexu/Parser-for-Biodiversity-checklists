eebffde553d481b7a3c6c437b7bd65ebc3483c44
qingyuexu <qingyuex@andrew.cmu.edu>
2017-03-23 20:24:49 +1030

    Add files via upload

--- Medium_extension.R
+++ Medium_extension.R
@@ -4,43 +4,49 @@ library(stringr)
 
 url = 'http://ftp.funet.fi/pub/sci/bio/life/insecta/lepidoptera/ditrysia/papilionoidea/papilionidae/papilioninae/lamproptera/'
 
-#html_nodes selection please refer to selectorgadget.com
-species_line = url %>% 
-  read_html() %>% 
+# html_nodes selection please refer to selectorgadget.com
+species_line = url %>%
+  read_html() %>%
   html_nodes(".SP .SN > li") %>% html_text()
 
-sciname <- gsub("\\;.*","",species_line)
-author_year <- gsub(".*  ","",sciname)
-Author<-gsub(",.*","",author_year)
-year<-str_trim(gsub(".*,","",author_year))
-for(i in 1:length(year)){
-  if(str_detect(year[i],"\\[.*\\]")){
-  Year<-str_replace(year,"\\[.*\\]",str_sub(year,2,5))
+# get the text from needed part
+sciname <- gsub("\\;.*", "", species_line)
+
+# get author and year
+author_year <- gsub(".*  ", "", sciname)
+Author <- gsub(",.*", "", author_year)
+year <- str_trim(gsub(".*,", "", author_year))
+for (i in 1:length(year)) {
+  if (str_detect(year[i], "\\[.*\\]")) {
+    Year <- str_replace(year, "\\[.*\\]", str_sub(year, 2, 5))
   }
 }
 
-Genus<-c()
-Species<-c()
-Subspecies<-c()
-sci<-gsub("  .*","",sciname)
-sci_split<-str_split(sci," ")
-for(i in 1:length(sci_split)){
-  if(length(sci_split[[i]])==2){
-    Genus[i]<-sci_split[[i]][1]
-    Species[i]<-sci_split[[i]][2]
-    Subspecies[i]<-"NA"
+# get Genus, Species and subspecies
+Genus <- c()
+Species <- c()
+Subspecies <- c()
+sci <- gsub("  .*", "", sciname)
+sci_split <- str_split(sci, " ")
+for (i in 1:length(sci_split)) {
+  if (length(sci_split[[i]]) == 2) {
+    Genus[i] <- sci_split[[i]][1]
+    Species[i] <- sci_split[[i]][2]
+    Subspecies[i] <- "NA"
   }
-  if(length(sci_split[[i]])==3){
-    Genus[i]<-sci_split[[i]][1]
-    Species[i]<-sci_split[[i]][2]
-    Subspecies[i]<-sci_split[[i]][3]
+  if (length(sci_split[[i]]) == 3) {
+    Genus[i] <- sci_split[[i]][1]
+    Species[i] <- sci_split[[i]][2]
+    Subspecies[i] <- sci_split[[i]][3]
   }
 }
 
-table<-cbind(as.data.frame(Genus),
-             as.data.frame(Species),
-             as.data.frame(Subspecies),
-             as.data.frame(Author),
-             as.data.frame(Year))
-
+# Organize results and output to file
+table <- cbind(
+  as.data.frame(Genus),
+  as.data.frame(Species),
+  as.data.frame(Subspecies),
+  as.data.frame(Author),
+  as.data.frame(Year)
+)
 write.csv(table, file = "Medium_extension.csv", row.names = T)
